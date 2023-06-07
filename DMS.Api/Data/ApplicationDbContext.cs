76520291718ade5e25fb12111543d549bb1bb4a2
Orlando Murcio <ato.murcio@gmail.com>
2023-06-07 11:44:16 -0600

    Add connection string configuration

---
+++ DMS.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DMS.Api;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+    }
+}
+
