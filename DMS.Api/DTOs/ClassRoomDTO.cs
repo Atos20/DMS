10460e671a19112eddfb0c4ae6a7852d90850113
Orlando Murcio <ato.murcio@gmail.com>
2023-06-08 16:23:33 -0600

    Create classroom contract

---
+++ DMS.Api/DTOs/ClassRoomDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DMS.Api.DTOs
+{
+	public class ClassRoomDTO
+	{
+		public ClassRoomDTO()
+		{
+		}
+
+        public int ClassRoomId { get; set; }
+        public string? ClassRoomName { get; set; }
+        public string? CourseName { get; set; }
+        public string? ChildCareWorker { get; set; }
+        public int ChildrenLimit { get; set; }
+        public int StartAge { get; set; }
+        public int EndAge { get; set; }
+    }
+}
+
