ca2c16b818a87c611854cc11f16e680debd155f8
Orlando Murcio <ato.murcio@gmail.com>
2023-06-08 16:22:58 -0600

    Define classrrom DTO

--- DMS.Api/DTOs/SchoolDTO.cs
+++ DMS.Api/DTOs/SchoolDTO.cs
@@ -3,7 +3,6 @@ namespace DMS.Api.DTOs
 {
 	public class SchoolDTO
 	{
-		
 		public int SchoolId { get; set; }
 		public string? SchoolName { get; set; }
 		public string? DirectorName { get; set; }
