0d01652340381d6005aaf26cd54f396234ec5b6d
Orlando Murcio <ato.murcio@gmail.com>
2023-06-08 20:41:45 -0600

    Declare get class details contract

--- DMS.Api/Repositories/SchoolRepository.cs
+++ DMS.Api/Repositories/SchoolRepository.cs
@@ -17,6 +17,37 @@ namespace DMS.Api.Repositories
             _logger = logger;
         }
 
+        /// <summary>
+        ///  Gets All classrooms for a given school
+        /// </summary>
+        /// <param name="id">School Id</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<ClassRoom>> GetClassroomsBySchoolId(int? id)
+        {
+            try
+            {
+                var classrooms = await (
+                    from cr in _context.ClassRoom
+                    where cr.SchoolId == id
+                    select cr
+                    ).ToListAsync();
+
+                return classrooms;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it as needed
+                _logger.LogError(ex, "An error occurred while retrieving schools.");
+                throw;
+
+            }
+        }
+
+        /// <summary>
+        ///  Find the first element that matches the name
+        /// </summary>
+        /// <param name="string">Name of School</param>
+        /// <returns></returns>
         public async Task<School?> GetSchoolByName(string schoolName)
         {
             try
